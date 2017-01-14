4c456776b39ea597f323437a588b7690f3ee5a1b
Marcel Fraas <mfraas64@gmail.com>
2017-01-14 13:53:12 +0100

    Spring Boot Project generated

---
+++ simplechat-server/src/main/java/com/mfraas/SimplechatServerApplication.java
@@ -0,0 +1,12 @@
+package com.mfraas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplechatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplechatServerApplication.class, args);
+	}
+}
